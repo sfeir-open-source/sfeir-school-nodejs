c750c26445fbce9de244e2fc4ed5068916e6705f
Siegfried Ehret <siegfried@ehret.me>
2017-06-01 09:58:56 +0200

    :gift: update Sfeirpeople

--- sfeirpeople/lib/people.js
+++ sfeirpeople/lib/people.js
@@ -1,43 +1,9 @@
 const router = require('express').Router();
 
 const api = require('./api');
-const {getCol} = require('./db');
 
-router.get('/', function(req, res, next) {
-  console.log('ERROR !');
-  next(new Error('LE FAIL'));
-}, (req, res, next) => {
-  console.log(req);
-  getCol().find().toArray((err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end();
-    } else {
-      res.statusCode = 200;
-      res.json(data);
-    }
-  })
-});
-
-router.get('/name/:name', (req, res, next) => {
-  const name = req.params.name;
-  const query = {$or:[
-    {"firstname": name},
-    {"lastname": name}
-  ]};
-
-  console.log(query);
-  getCol().find(query).toArray((err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end();
-    } else {
-      res.statusCode = 200;
-      res.json(data);
-    }
-  });
-});
-
+router.get('/', api.listAll);
+router.get('/name/:name', api.filterByName);
 router.get('/random', api.getRandom);
 router.get('/:id', api.get);
 router.get('/skill/:skill', api.filterBySkill);
