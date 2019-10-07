b216186019383b578a93e2709ebb51ca267739ed
GBrunois <brunois.g@sfeir.com>
2019-10-07 18:46:36 +0200

    :fire: remove all solutions steps

--- project/solution/step01/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello, Sfeir School!'));
-
-app.listen(3000, () => console.log('App listening on port 3000!'));
--- project/solution/step03/index.js
+++
@@ -1,3 +0,0 @@
-const app = require('./lib/app');
-
-app.listen(3000, () => console.log('App listening on port 3000!'));
