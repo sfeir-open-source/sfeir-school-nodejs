69f565dce4bb6646a8626f0ac0c058cc65da9b7c
GBrunois <brunois.g@sfeir.com>
2021-10-18 23:03:59 +0200

    :zap: migrate reveal-js4

--- docs/scripts/dont-touch/reveal-config.js
+++ docs/scripts/dont-touch/reveal-config.js
@@ -1,83 +1,44 @@
+import Reveal from '../../web_modules/reveal.js/dist/reveal.esm.js';
+import RevealMarkdown from '../../web_modules/reveal.js/plugin/markdown/markdown.esm.js'
+import RevealZoom from '../../web_modules/reveal.js/plugin/zoom/zoom.esm.js';
+import RevealNotes from '../../web_modules/reveal.js/plugin/notes/notes.esm.js';
+import RevealHighLight from '../../web_modules/reveal.js/plugin/highlight/highlight.esm.js';
+import RevealSfeirTheme from '../../web_modules/sfeir-school-theme/dist/js/sfeir-theme.js';
 import { renderSlides } from './import-slides.js';
 import { usedSlides } from '../slides.js';
 
-(() => {
-  const importSlideElement = document.querySelector('.slides');
-  renderSlides(importSlideElement, usedSlides());
+const importSlideElement = document.querySelector('.slides');
+renderSlides(importSlideElement, usedSlides());
 
-  setTimeout(() =>
-    Reveal.initialize(
-      {
-        controls: true,
-        progress: true,
-        history: true,
-        center: false,
-        width: 1920,
-        height: 1080,
+Reveal.initialize(
+  {
+    controls: true,
+    progress: true,
+    history: true,
+    center: false,
+    width: 1920,
+    height: 1080,
 
-        theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-        transition: Reveal.getQueryHash().transition || 'none', // default/cube/page/concave/zoom/linear/fade/none
+    keyboard: {
+      32: function() {
+        var video = document.querySelector('.present video');
+        if (video.paused == true) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      }
+    },
 
-        keyboard: {
-          32: function() {
-            var video = document.querySelector('.present video');
-            if (video.paused == true) {
-              video.play();
-            } else {
-              video.pause();
-            }
-          }
-        },
+    slideNumber: 'c/t',
+    showSlideNumber: 'speaker',
+    plugins: [
+      RevealMarkdown, RevealSfeirTheme, RevealZoom, RevealNotes, RevealHighLight
+    ]
+  }).then(() => {
+    Reveal.configure({
+      theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+      transition: Reveal.getQueryHash().transition || 'none', // default/cube/page/concave/zoom/linear/fade/none
+    });
+  });
 
-        slideNumber: 'c/t',
-        showSlideNumber: 'speaker',
-
-        // Optional libraries used to extend on reveal.js
-        dependencies: [
-          {
-            src: './web_modules/reveal.js/js/classList.js',
-            condition: function() {
-              return !document.body.classList;
-            }
-          },
-          {
-            src: './web_modules/reveal.js/plugin/markdown/marked.js',
-            condition: function() {
-              return !!document.querySelector('[data-markdown]');
-            }
-          },
-          {
-            src: './web_modules/reveal.js/plugin/markdown/markdown.js',
-            condition: function() {
-              return !!document.querySelector('[data-markdown]');
-            }
-          },
-          {
-            src: './web_modules/reveal.js/plugin/highlight/highlight.js',
-            async: true,
-            callback: function() {
-              hljs.initHighlightingOnLoad();
-            }
-          },
-          {
-            src: './web_modules/reveal.js/plugin/zoom-js/zoom.js',
-            async: true,
-            condition: function() {
-              return !!document.body.classList;
-            }
-          },
-          {
-            src: './web_modules/reveal.js/plugin/notes/notes.js',
-            async: true,
-            condition: function() {
-              return !!document.body.classList;
-            }
-          }
-          // { src: 'node_modules/reveal.js/plugin/search/search.js', async: true, condition: function() { return !!document.body.classList; } }
-          // { src: 'node_modules/reveal.js/plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
-        ]
-      },
-      1000
-    )
-  );
-})();
