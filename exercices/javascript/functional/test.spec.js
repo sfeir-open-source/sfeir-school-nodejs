ad4fd5858daa188774f9634a7ef76a7b4e2e8395
Geoffrey Brunois <geoffrey.brunois@gmail.com>
2021-10-24 09:46:03 +0200

    :memo: tipo on javascript exercice

--- exercices/javascript/functional/test.spec.js
+++ exercices/javascript/functional/test.spec.js
@@ -1,11 +1,7 @@
 const { totalAmount } = require('./exo.js');
 
 describe('Test exercise for functional Exercice 1', function() {
+
   it('Is create correctly', () => expect(totalAmount).toEqual(385));
-});
-
-const { totalAmountPipe } = require('./exo2.js');
-
-describe('Test exercise for functional Exercice 2', function() {
-  it('Is create correctly', () => expect(totalAmountPipe).toEqual(385));
+  
 });
